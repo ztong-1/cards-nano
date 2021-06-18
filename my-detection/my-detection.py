b272a6c796f27293e9b10d0886c90ba1b2a2fe94
ztong4 <ztong4@ncsu.edu>
2021-06-17 23:28:03 -0400

    overlay text to img

--- my-detection/my-detection.py
+++ my-detection/my-detection.py
@@ -1,19 +1,24 @@
 import jetson.inference
 import jetson.utils
 import sys
-from twenty_four_game import Solution
+from twenty_four_game import Solution2
 
 print("Print system arguments...")
 for a in sys.argv:
     print(a)
 
+# load the detection network
 net = jetson.inference.detectNet(argv = sys.argv)
+
+# create video sources and outputs
 camera = jetson.utils.videoSource("/dev/video0")
 display = jetson.utils.videoOutput()
+font1 = jetson.utils.cudaFont()
+font2 = jetson.utils.cudaFont()
 
-s = Solution()
+s = Solution2()
 
-curr_sum = -1
+res = "No solution"
 curr_det = []
 
 while True:
@@ -25,23 +30,30 @@ while True:
         temp_det.sort()
     else:
         temp_det = []
-
+    
+    font1.OverlayText(img, img.width, img.height, "Detected: " + str(temp_det), 5, 5, font1.White, font1.Gray40)
     if temp_det != curr_det:
         print("Detected:", temp_det)
-
+    
     if len(temp_det) == 4:
-        #temp_sum = sum(temp_det)
         #if temp_det != curr_det:
-            #print("temp_sum", temp_sum)
-        #curr_sum = temp_sum
+            #res = s.judgePoint24(temp_det)
+            #if res == False:
+                #print("No solution exists.")
         if temp_det != curr_det:
             res = s.judgePoint24(temp_det)
-            if res == False:
-                print("No solution exists.")
-
+            print(res)
+    else:
+        res = "No solution"
+                
+    font2.OverlayText(img, img.width, img.height, res, 5, 40, font2.White, font2.Gray40)
+    
     curr_det = temp_det
-
+    
+    # render the image
     display.Render(img)
+    
+    # update the title bar
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
     
 
--- my-detection/twenty_four_game.py
+++ my-detection/twenty_four_game.py
@@ -164,8 +164,172 @@ class Solution:
 
 
 #s = Solution()
-#res = s.judgePoint24([5,1,1,5])
+#res = s.judgePoint24([2,3,4,1])
 #if res == False:
 #    print("No solution exists.")
+#print("\n\n")
+
+class Solution2:
+    def judgePoint24(self, cards:[int]) -> str:
+        def process_ops(ops):
+            res = "(" + ops[-1][1] + ")"
+            for i in range(len(ops)-2, -1, -1):
+                if "+" in ops[i][1] or "-" in ops[i][1]:
+                    curr_exp = "(" + ops[i][1] + ")"
+                else:
+                    curr_exp = ops[i][1]
+                    
+                if ops[i][0] == "+":
+                    res = res + "-" + curr_exp
+                elif ops[i][0] == ".-":
+                    res = curr_exp + "-" + res
+                elif ops[i][0] == "-.":
+                    res = curr_exp + "+" + res
+                elif ops[i][0] == "*":
+                    res = res + "/" + curr_exp
+                elif ops[i][0] == "./":
+                    res = curr_exp + "/" + res
+                else:
+                    res = curr_exp + "*" + res
+                if i != 0:
+                    res = "(" + res + ")"
+            return res + " = 24."
+
+        # x is numerator, y is denominator
+        def dfs(x, y, remain, ops):
+            #print("x", x, "y", y, "remain:", remain, ops)
+            if len(remain) == 2:
+                x_y = x / y
+                temp = remain[0] + remain[1]
+                if temp == x_y:
+                    res = process_ops(ops + [("", str(remain[0])+"+"+str(remain[1]))])
+                    #print(res)
+                    return res
+                temp = remain[0] - remain[1]
+                if temp == x_y:
+                    res = process_ops(ops + [("", str(remain[0])+"-"+str(remain[1]))])
+                    #print(res)
+                    return res
+                temp = remain[1] - remain[0]
+                if temp == x_y:
+                    res = process_ops(ops + [("", str(remain[1])+"-"+str(remain[0]))])
+                    #print(res)
+                    return res
+                temp = remain[1] * remain[0]
+                if temp == x_y:
+                    res = process_ops(ops + [("", str(remain[0])+"*"+str(remain[1]))])
+                    #print(res)
+                    return res
+                temp = remain[0]/remain[1]
+                if temp == x_y:
+                    res = process_ops(ops + [("", str(remain[0])+"/"+str(remain[1]))])
+                    #print(res)
+                    return res
+                temp = remain[1]/remain[0]
+                if temp == x_y:
+                    res = process_ops(ops + [("", str(remain[1])+"/"+str(remain[0]))])
+                    #print(res)
+                    return res
+                return "No solution"
+                
+            for i in range(len(remain)):
+                r = remain[i]
+                # x/y - r
+                new_x, new_y = x - r*y, y
+                temp = dfs(new_x, new_y, remain[:i] + remain[(i+1):], ops+[("-.",str(r))])
+                #print("remain:", remain," temp:",temp)
+                if temp != "No solution":
+                    return temp
+                # x/y + r
+                new_x, new_y = x + r*y, y
+                temp = dfs(new_x, new_y, remain[:i] + remain[(i+1):], ops+[("+",str(r))])
+                if temp != "No solution":
+                    return temp
+                # r - x/y
+                new_x, new_y = r*y - x, y
+                temp = dfs(new_x, new_y, remain[:i] + remain[(i+1):], ops+[(".-",str(r))])
+                if temp != "No solution":
+                    return temp
+                # (x/y) / r
+                new_x, new_y = x, y*r 
+                temp = dfs(new_x, new_y, remain[:i] + remain[(i+1):], ops+[("/.",str(r))])
+                if temp != "No solution":
+                    return temp
+                # r / (x/y)
+                new_x, new_y = r*y, x
+                temp = dfs(new_x, new_y, remain[:i] + remain[(i+1):], ops+[("./",str(r))])
+                if temp != "No solution":
+                    return temp
+                # r * (x/y)
+                new_x, new_y = r*x, y
+                temp = dfs(new_x, new_y, remain[:i] + remain[(i+1):], ops+[("*",str(r))])
+                if temp != "No solution":
+                    return temp
+                    
+            return "No solution"
+
+            if len(remain) == 4:    
+                for i in range(3):
+                    for j in range(i+1,4):
+                        c1, c2 = cards[i], cards[j]
+                        comp = list(set(range(4)) - set([i,j])) # complement
+                        c3, c4 = cards[comp[0]], cards[comp[1]]
+                        temp_xy = []
+                        temp_xy.append((c1+c2,1))
+                        temp_xy.append((c1-c2,1))
+                        temp_xy.append((c2-c1,1))
+                        temp_xy.append((c1*c2,1))
+                        temp_xy.append((c1,c2))
+                        temp_xy.append((c2,c1))
+                        temp_ops = []
+                        temp_ops.append(str(c1)+"+"+str(c2))
+                        temp_ops.append(str(c1)+"-"+str(c2))
+                        temp_ops.append(str(c2)+"-"+str(c1))
+                        temp_ops.append(str(c1)+"*"+str(c2))
+                        temp_ops.append(str(c1)+"/"+str(c2))
+                        temp_ops.append(str(c2)+"/"+str(c1))
+                        for k in range(6):
+                            new_x, new_y = temp_xy[k][0]+24*temp_xy[k][1], temp_xy[k][1]
+                            temp = dfs(new_x, new_y, [c3, c4], ops+[("+", temp_ops[k])])
+                            if temp != "No solution":
+                                return temp
+                            new_x, new_y = temp_xy[k][0]-24*temp_xy[k][1], temp_xy[k][1]
+                            temp = dfs(new_x, new_y, [c3, c4], ops+[(".-", temp_ops[k])])
+                            if temp != "No solution":
+                                return temp
+                            new_x, new_y = 24*temp_xy[k][1]-temp_xy[k][0], temp_xy[k][1]
+                            temp = dfs(new_x, new_y, [c3, c4], ops+[("-.", temp_ops[k])])
+                            if temp != "No solution":
+                                return temp
+                            
+                            if temp_xy[k][0] != 0:
+                                new_x, new_y = 24*temp_xy[k][0], temp_xy[k][1]
+                                temp = dfs(new_x, new_y, [c3, c4], ops+[("*", temp_ops[k])])
+                                if temp != "No solution":
+                                    return temp
+                            if temp_xy[k][0] != 0:
+                                new_x, new_y = temp_xy[k][0], 24*temp_xy[k][1]
+                                temp = dfs(new_x, new_y, [c3, c4], ops+[("./", temp_ops[k])])
+                                if temp != "No solution":
+                                    return temp
+                            if temp_xy[k][0] != 0:
+                                new_x, new_y = 24*temp_xy[k][1], temp_xy[k][0]
+                                temp = dfs(new_x, new_y, [c3, c4], ops+[("/.", temp_ops[k])])
+                                if temp != "No solution":
+                                    return temp
+                
+                return "No solution"
+        
+        res = dfs(24, 1, cards, [])
+        return res
+
+
+#s = Solution2()
+#res = s.judgePoint24([2,3,4,1])
+#print(res)
+
+
+
+
 
 
